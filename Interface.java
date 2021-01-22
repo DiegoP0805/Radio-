8f1653181edb5b389fa3fe5d879a516915b1c14d
DiegoP0805 <60204933+DiegoP0805@users.noreply.github.com>
2021-01-21 21:41:29 -0600

    Add files via upload

--- Interface.java
+++ Interface.java
@@ -1,15 +1,43 @@
-/*
-Programadores: Diego Perdomo Sagastume y Ana Gabriela Ramírez y resto de la sección 30
-Nombre del programa: Interface.java
-Lengaje: Java
-Fecha de modificación: 17/01/21
-*/
+/**
+ * Interfaz con el fin de crear parámetros comunes para los estudiantes 
+ * @since 14/01/2021
+ */
+
 public interface Interface 
 {
-	//Se muestra la estructura de métodos básicos para radio.java 
+    /** 
+     * Este método pretende servir para cmabiar el estado de apagado o encendido de la radio
+     * @return boolean
+     */
     public boolean encenderApagar();
+
+
+    /** 
+     * Este método tiene como función cambiar la frecuencia de la radio (AM a Fm y viceversa)
+     * @return boolean
+     */
     public boolean amFm();
+
+    
+    /** 
+     * Este método sirve para guardar una estación en alguno de los botones
+     * @param btn
+     * @return String
+     */
     public String guardar(int btn);
+    
+    
+    /** 
+     * Método que permite seleccionar uno de los botones y sintonizar la estación guardada en este
+     * @param btn
+     * @return String
+     */
     public String seleccionar(int btn);
+
+
+    /** 
+     * Este método permite avanzar en las diferentes estaciones de las frecuencias
+     * @return double
+     */
     public double avanzar();
 }
--- radio.java
+++ radio.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 Programadores: Diego Perdomo Sagastume y Ana Gabriela Ramírez
 Nombre del programa: radio.java (depende de Interface.java)
 Lengaje: Java
-Fecha de modificación: 17/01/21
+Fecha de modificación: 16/01/21
 */
 public class radio implements Interface
 {
@@ -14,6 +14,10 @@ public class radio implements Interface
     public boolean frecuencia;//am o fm
     public Double[] botones;
 
+    
+    /**
+     * Setter de la variable onOff
+     */
     public void setonOff()//metodo para encender y apagar la radio
     {
         if(onOff)
@@ -26,12 +30,20 @@ public class radio implements Interface
         }
     }
 
+    
+    /** 
+     * Getter de la variable onOff
+     * @return boolean
+     */
     public boolean getonOff()
     {
         return onOff;
     }
 
 
+    /**
+     * Setter de la variable frecuencia
+     */
     public void setfrecuencia()//metodo para cambiar de precuencia
     {
         if(frecuencia)
@@ -44,11 +56,24 @@ public class radio implements Interface
         }
     }
 
+    
+    /** 
+     * Getter de la variable de la frecuencia
+     * @return boolean
+     */
     public boolean getamFm()//devuelve la frecuencia como un boolean
     {
         return frecuencia;
     }
 
+    
+    /** 
+     * Este método tiene como función cambiar la frecuencia de la radio (AM a Fm y viceversa)
+     * @return boolean
+     * @see getamFm()
+     * @see setEstacion()
+     * @see setfrecuencia()
+     */
     @Override
     public boolean amFm()//metodo para cambiar la frecuencia
     {
@@ -65,23 +90,46 @@ public class radio implements Interface
         return getamFm();
     }
 
+    
+    /** 
+     * Este método pretende servir para cmabiar el estado de apagado o encendido de la radio
+     * @return boolean
+     * @see setonOff()
+     */
     @Override
-    public boolean encenderApagar()//metodo para apagar o encender
+    public boolean encenderApagar()
     {
         setonOff();    
         return getonOff();
     }
 
+    
+    /** 
+     * @param estacion
+     */
     public void setEstacion(double estacion) 
     {
         this.estacion = estacion;
     }
 
+    
+    /** 
+     * Getter de la estación
+     * @return double
+     */
     public double getEstacion() 
     {
         return estacion;
     }
 
+    
+    /** 
+     * Este método permite avanzar en las diferentes estaciones de las frecuencias
+     * @return double
+     * @see getamFm()
+     * @see setEstacion()
+     * @see getEstacion()
+     */
     @Override
     public double avanzar()//metodo para avanzar entre emisoras
     {
@@ -105,7 +153,10 @@ public class radio implements Interface
     }
 
 
-    public radio()//se le otorga valores a las variables de radio
+    /**
+     * Este método sirve como el constructor 
+     */
+    public radio()
     {
         onOff = false;
         estacion = 0.0;
@@ -113,17 +164,34 @@ public class radio implements Interface
         botones =  new Double[]{0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0};
     }
 
+    
+    /** 
+     * Este método sirve para guardar una estación en alguno de los botones
+     * @param btn
+     * @return String
+     * @see getEstacion()
+     */
     @Override
-    public String guardar(int btn) //metodo para guarrdar una estación en un botón, funciona con array
+    public String guardar(int btn) 
     {
         botones[btn] = getEstacion();
         return Double.toString(botones[btn]);
     }
 
+    
+    /** 
+     * Método que permite seleccionar uno de los botones y sintonizar la estación guardada en este
+     * @param btn
+     * @return String
+     * @see getamFm()
+     * @see setEstacion()
+     * @see setfrecuencia()
+     * @see getEstacion()
+     */
     @Override
-    public String seleccionar(int btn) //metodo validador para cuando se quiera seleccionar un botón para sintonizar una emisora
+    public String seleccionar(int btn) 
     {
-        if(botones[btn] >= 87.9 && botones[btn] <= 107.9)//para fm
+        if(botones[btn] >= 87.9 && botones[btn] <= 107.9)
         {
             if(getamFm())
             {
@@ -138,7 +206,7 @@ public class radio implements Interface
             }
             
         }
-        else if(botones[btn] >= 530.0 && botones[btn] <= 1610.0)//para am
+        else if(botones[btn] >= 530.0 && botones[btn] <= 1610.0)
         {
             if(getamFm())
             {
@@ -152,7 +220,7 @@ public class radio implements Interface
                 return Double.toString(getEstacion());
             }
         }
-        else//no se puede sintonizar la estación de un botón que no tiene nada guardado
+        else
         {
             return "No hay estacion guardada en este boton";
         }
